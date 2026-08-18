ec10408cf73bf88eaab9ee915fb1ba3297f47a9f
Maria Clara <mariaclaracorecha18@gmail.com>
2026-08-18 15:47:37 -0300

    Feat: Remove Book, Customer, Rental entities and related components

--- biblioteca-api/src/main/java/com/biblioteca/biblioteca_api/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.biblioteca.biblioteca_api.repository;
-
-import com.biblioteca.biblioteca_api.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
